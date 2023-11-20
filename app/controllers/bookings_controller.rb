a3cc85a6f83759cbf80fb3abae4769a70d28b333
Tiphaine Ollivier <tiphaine.ollivier1@gmail.com>
2023-11-20 21:14:11 +0100

    linked the flashes scss in the index + confirmed the new booking work + started working on the booking show + moved the navbar & footer outside of the container

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @bookings = Booking.all
   end
@@ -11,18 +11,23 @@ class BookingsController < ApplicationController
   end
 
   def create
+    start_date, end_date = params[:booking][:start_date].split("to")
     @dimension = Dimension.find(params[:dimension_id])
     @booking = Booking.new(bookings_params)
+    @booking.start_date = start_date
+    @booking.end_date = end_date
     @booking.dimension = @dimension
     @booking.user = current_user
+
     if @booking.save
-      redirect_to booking_path, :notice => "Successfully created booking."
+      redirect_to booking_path(@booking.id), notice: 'Booking was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
+    @bookings = Booking.all
     @booking = Booking.find(params[:id])
   end
 

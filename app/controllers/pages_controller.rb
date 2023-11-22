313f9806236f671fb2828feab2ae28978e0b47f3
Federico Palou <federicopalou@gmail.com>
2023-11-22 13:32:58 +0100

    Changing the body, commiting to be up to date with master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     @categories = Category.all
+    @dimensions = Dimension.all
+    render :layout => false
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'bookings/new'
   get 'bookings/show'
-  root to: "pages#home"
+  # root to: "pages#home"
+  root to: redirect('/home')
+  get 'home', to: 'pages#home'
   devise_for :users, controllers: { profiles: 'profiles' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -14,5 +16,4 @@ Rails.application.routes.draw do
   resources :bookings, only: %i[show destroy]
   ## Custom routes for profile
   resource :profile, only: [:show]
-
 end
